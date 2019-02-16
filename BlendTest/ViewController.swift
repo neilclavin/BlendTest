44e163def0eb09438e04af6d96242f3b869d68b9
Neil Clavin <neil@neils-macbook-pro.local>
2019-02-16 20:30:01 +0100

    Version

--- BlendTest/ViewController.swift
+++ BlendTest/ViewController.swift
@@ -9,12 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var image1: UIImageView!
+    
+    @IBOutlet weak var image2: UIImageView!
+    
+    @IBOutlet weak var resultImage: UIImageView!
+    
+    
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var img2: UIImageView!
+    
+    func blendImage(chooseImage: UIImageView) {
+        if  let img = UIImage(named: "lucaA"), let img2 = UIImage(named: "lucaB") {
+            let rect = CGRect(x: 0, y: 0, width: img.size.width, height: img.size.height)
+            let renderer = UIGraphicsImageRenderer(size: img.size)
+            
+           let result = renderer.image { ctx in
+                // fill the background with white so that translucent colors get lighter
+                UIColor.white.set()
+                ctx.fill(rect)
+                
+                img.draw(in: rect, blendMode: .normal, alpha: 1)
+                img2.draw(in: rect, blendMode: .multiply, alpha: 1)
+            
+           
+    
+            }
+             resultImage.image = result
+            
+        }
+    }
+    
+        
+        
+    
+        
+    
+        override func viewDidLoad() {
+            super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+            blendImage (chooseImage: resultImage)
     }
 
 
 }
+
+
 
